81e7924091dc7bf54d94b23c9d8274854e99aba6
kaiqi-ken <1075943618@qq.com>
2025-02-12 23:31:23 +0800

    [bugfix] 1. remove redundant import from __init__

--- se3dif/datasets/__init__.py
+++ se3dif/datasets/__init__.py
@@ -1,4 +1,2 @@
 from .acronym_dataset import AcronymGrasps, AcronymGraspsDirectory, AcronymAndSDFDataset,\
                              PointcloudAcronymAndSDFDataset, PartialPointcloudAcronymAndSDFDataset
-
-from .task_grasp_dataset import GCNTaskGrasp, TASKS
